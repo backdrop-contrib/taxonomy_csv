461faca777c58e744b5ecf9f0b191ff57d6fe91a
Daniel Berthereau <daniel_km@428555.no-reply.drupal.org>
2009-08-11 11:52:16 +0000

    #540916 by Daniel Berthereau: Added warning to disable Pathauto before import.

--- taxonomy_csv.js
+++ taxonomy_csv.js
@@ -1,82 +1,86 @@
 // $Id$
 
-/**
- * Allows to hide unchosen sub-options and to show chosen sub-options.
- *
- * TODO: To be factorized.
- */
 if (Drupal.jsEnabled) {
-  $(document).ready(function() {
-    // Source options.
-    $('#source').addClass('filtered');
-    // Source choice.
-    // Add/remove class to show/hide it.
-    $('#source_choice').change(function(){
-      var methods = new Array('textarea', 'local_file');
-      for(var m in methods) {
-        $('#source').removeClass(methods[m]);
-      }
-      $('#source').addClass(this.value)
-        .animate({opacity:.5}, 1)
-        .animate({opacity:1}, 1)
-    });
-    // Update current display.
-    $('#source_choice').trigger('change');
+  (function ($) {
 
-    // Source delimiter.
-    // Add/remove class to show/hide it.
-    $('#source_delimiter').change(function(){
-      var methods = new Array('comma', 'semicolon', 'tabulation', 'space', 'custom_delimiter');
-      for(var m in methods) {
-        $('#source').removeClass(methods[m]);
-      }
-      $('#source').addClass(this.value)
-        .animate({opacity:.5}, 1)
-        .animate({opacity:1}, 1)
-    });
-    // Update current display.
-    $('#source_delimiter').trigger('change');
+    /**
+    * Allows to hide unchosen sub-options and to show chosen sub-options.
+    *
+    * TODO: To be factorized.
+    */
+    $(document).ready(function() {
+      // Source options.
+      $('#source').addClass('filtered');
+      // Source choice.
+      // Add/remove class to show/hide it.
+      $('#source_choice').change(function(){
+        var methods = new Array('textarea', 'local_file');
+        for(var m in methods) {
+          $('#source').removeClass(methods[m]);
+        }
+        $('#source').addClass(this.value)
+          .animate({opacity:.5}, 1)
+          .animate({opacity:1}, 1)
+      });
+      // Update current display.
+      $('#source_choice').trigger('change');
 
-    // Source enclosure.
-    // Add/remove class to show/hide it.
-    $('#source_enclosure').change(function(){
-      var methods = new Array('none', 'quotation', 'custom_enclosure');
-      for(var m in methods) {
-        $('#source').removeClass(methods[m]);
-      }
-      $('#source').addClass(this.value)
-        .animate({opacity:.5}, 1)
-        .animate({opacity:1}, 1)
-    });
-    // Update current display.
-    $('#source_enclosure').trigger('change');
+      // Source delimiter.
+      // Add/remove class to show/hide it.
+      $('#source_delimiter').change(function(){
+        var methods = new Array('comma', 'semicolon', 'tabulation', 'space', 'custom_delimiter');
+        for(var m in methods) {
+          $('#source').removeClass(methods[m]);
+        }
+        $('#source').addClass(this.value)
+          .animate({opacity:.5}, 1)
+          .animate({opacity:1}, 1)
+      });
+      // Update current display.
+      $('#source_delimiter').trigger('change');
 
-    // Destination choice.
-    $('#destination').addClass('filtered');
-    // Add/remove class to show/hide it.
-    $('#destination_target').change(function(){
-      var methods = new Array('autocreate', 'duplicate', 'existing');
-      for(var m in methods) {
-        $('#destination').removeClass(methods[m]);
-      }
-      $('#destination').addClass(this.value)
-        .animate({opacity:.5}, 1)
-        .animate({opacity:1}, 1)
-    });
-    // Update current display.
-    $('#destination_target').trigger('change');
+      // Source enclosure.
+      // Add/remove class to show/hide it.
+      $('#source_enclosure').change(function(){
+        var methods = new Array('none', 'quotation', 'custom_enclosure');
+        for(var m in methods) {
+          $('#source').removeClass(methods[m]);
+        }
+        $('#source').addClass(this.value)
+          .animate({opacity:.5}, 1)
+          .animate({opacity:1}, 1)
+      });
+      // Update current display.
+      $('#source_enclosure').trigger('change');
 
-    // Autodivide choice.
-    // Add/remove class to show/hide it.
-    $('#edit-autodivide-import').change(function(){
-      if(this.checked) {
-        $('#autodivide').removeClass('filtered');
-      }
-      else {
-        $('#autodivide').addClass('filtered');
-      }
+      // Destination choice.
+      $('#destination').addClass('filtered');
+      // Add/remove class to show/hide it.
+      $('#destination_target').change(function(){
+        var methods = new Array('autocreate', 'duplicate', 'existing');
+        for(var m in methods) {
+          $('#destination').removeClass(methods[m]);
+        }
+        $('#destination').addClass(this.value)
+          .animate({opacity:.5}, 1)
+          .animate({opacity:1}, 1)
+      });
+      // Update current display.
+      $('#destination_target').trigger('change');
+
+      // Autodivide choice.
+      // Add/remove class to show/hide it.
+      $('#edit-autodivide-import').change(function(){
+        if(this.checked) {
+          $('#autodivide').removeClass('filtered');
+        }
+        else {
+          $('#autodivide').addClass('filtered');
+        }
+      });
+      // Update current display.
+      $('#edit-autodivide-import').trigger('change');
     });
-    // Update current display.
-    $('#edit-autodivide-import').trigger('change');
- });
+
+  })(jQuery);
 }
